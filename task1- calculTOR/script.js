887907f562af7fed8ada7016294dbc46c22fcc6e
kaash_lai <96759025+kaashlai@users.noreply.github.com>
2024-04-03 10:50:21 +0530

    done
    this calculator is used for simple calculating.
    it made up html along with js and css.

---
+++ task1- calculTOR/script.js
@@ -0,0 +1,16 @@
+// let string = "";
+// let button = document.querySelectorAll("button");
+// Array.from(button).forEach((button)=>{
+//     button.addEventListener("click", (e)=>{
+//         console.log(e.target)
+//         string += e.target.innerHTML;
+//         document.querySelector("input").value = string;
+//         // if(e.innerHTML == '='){
+//         //         string = eval(string);
+//         //         document.querySelector("input").value = string;
+//         // }
+//         // else{
+          
+//         // }
+//     });
+// });
